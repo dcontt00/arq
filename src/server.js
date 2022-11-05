3c5786143567d0a72dd31ebc5e62e680e43d01f8
ruben <rreisr00@estudiantes.unileon.es>
2022-11-05 16:47:21 +0100

    Added Auth route

--- src/server.js
+++ src/server.js
@@ -1,33 +1,13 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 
-let people = [
-    {
-      name: "Hannah Rickard",
-      number: "06-51-99-56-83",
-      id: 1
-    },
-    {
-      name: "Hyun Namkoong",
-      number: "10987654",
-      id: 2
-    },
-    {
-      name: "Courtney Martinez",
-      number: "3691215",
-      id: 3
-    }
-  ]
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-  app.get('/', (request, response) => {
-      response.send('<h1>Phonebook</h1>')
-  })
+app.use("/api",require("./routes/login"))
 
-  app.get('/api/people', (request, response) => {
-      response.json(people)
-  })
-
-  const PORT = 3001
-  app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`)
-  })
+const PORT = 3001;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+});
