27031e9e8c7335aa5fb26b6c74415cc970652b28
dflorr00 <dflorr00@estudiantes.unileon.es>
2022-11-06 17:38:44 +0100

    Creado el archivo theme e implementado para poder modificar la apariencia general de la aplicacion desde un unico archivo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import * as React from 'react';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
 import { BrowserRouter as Router, Navigate, Routes, Route} from 'react-router-dom';
 import Login from './pages/login';
 import Home from './pages/home';
 
 export default function App(){
   return(
-    <Router>
-      <div>
-        <Routes>
-          <Route path='/' element={<Navigate to='/login' />}></Route>
-          <Route path='/login' element={<Login />}></Route>
-          <Route path='/home' element={<Home />}></Route>
-        </Routes>
-      </div>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <div>
+          <Routes>
+            <Route path='/' element={<Navigate to='/login' />}></Route>
+            <Route path='/login' element={<Login />}></Route>
+            <Route path='/home' element={<Home />}></Route>
+          </Routes>
+        </div>
+      </Router>
+    </ThemeProvider>
   );
 }
