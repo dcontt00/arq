ea84e89d5d79c604cb0a67fe5f69429d27ad90c5
ruben <rreisr00@estudiantes.unileon.es>
2022-11-07 10:29:39 +0100

    Added directory

--- src/routes/control_router.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.post("/control", function (req, res){
-    data = req.body;
-    //Use data to set variables TODO: Function to handle variables control
-});
-
-module.exports = router;
