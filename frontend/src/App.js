6f6fbe568a37d9540a866b3e417e4f45a903212d
dcontt00 <dcontt00@estudiantes.unielon.es>
2022-11-21 11:00:06 +0100

    Cambios en appbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,26 @@
-import * as React from 'react';
-import { ThemeProvider } from '@mui/material/styles';
-import theme from './theme';
-import { BrowserRouter as Router, Navigate, Routes, Route} from 'react-router-dom';
-import Login from './pages/login';
-import Home from './pages/home';
+import * as React from "react";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./theme";
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Login from "./pages/login";
+import Home from "./pages/home";
+import Appbar from "./components/Appbar";
 
-export default function App(){
-  return(
+export default function App() {
+  return (
     <ThemeProvider theme={theme}>
       <Router>
-          <Routes>
-            <Route path='/' element={<Navigate to='/login' />}></Route>
-            <Route path='/login' element={<Login />}></Route>
-            <Route path='/home' element={<Home />}></Route>
-          </Routes>
+        <Appbar />
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/home" element={<Home />}></Route>
+        </Routes>
       </Router>
     </ThemeProvider>
   );
--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,38 +1,25 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Grid from '@mui/material/Grid';
-import { CardContent, Typography } from '@mui/material';
-import Card from '@mui/material/Card';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Grid from "@mui/material/Grid";
+import { CardContent, Typography } from "@mui/material";
+import Card from "@mui/material/Card";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
 
-export default function Home(){
-
-    return (
-        <div>
-            {/*Barra del Menu*/}
-            <AppBar position="static"><Typography color="white">
-                <Toolbar>
-                    {/*0CA712*/}
-                    <Stack spacing={2} direction="row">
-                        <Button variant="contained" sx={{fontSize:'22'}}>Inicio</Button>
-                        <Button variant="contained" sx={{fontSize:'22'}}>Datos</Button>
-                        <Button variant="contained" sx={{fontSize:'22'}}>Control</Button>
-                        <Button variant="contained" sx={{fontSize:'22'}}>Cámara</Button>
-                    </Stack>
-                </Toolbar>
-            </Typography></AppBar>
-
-            {/*Parte de las Graficas*/}
-            <Grid container sx={{paddingTop:"2%",backgroundColor:'red'}}>
-                <Grid item lg={5.5}>
-                    <Card><CardContent>Tarjeta de prueba</CardContent></Card>
-                </Grid>
-                <Grid item lg={5.5}>
-                    <Card><CardContent>Tarjeta de prueba</CardContent></Card>
-                </Grid>
-            </Grid>
-        </div>
-    );
+export default function Home() {
+  return (
+    <Grid container sx={{ backgroundColor: "red" }}>
+      <Grid item lg={6}>
+        <Card>
+          <CardContent>Tarjeta de prueba</CardContent>
+        </Card>
+      </Grid>
+      <Grid item lg={6}>
+        <Card>
+          <CardContent>Tarjeta de prueba</CardContent>
+        </Card>
+      </Grid>
+    </Grid>
+  );
 }
