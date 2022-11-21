d1268dbc16c92265d1d6850a3d507493a4128c33
dcontt00 <dcontt00@estudiantes.unileon.es>
2022-11-21 15:37:57 +0100

    Created class for dht11

--- backend/src/dht11.py
+++ backend/src/dht11.py
@@ -3,23 +3,16 @@ import board
 import adafruit_dht
 import psutil
 
-PIN=board.D4
 
-# We first check if a libgpiod process is running. If yes, we kill it!
-for proc in psutil.process_iter():
-    if proc.name() == "libgpiod_pulsein" or proc.name() == "libgpiod_pulsei":
-        proc.kill()
-sensor = adafruit_dht.DHT11(PIN)
-while True:
-    try:
-        temp = sensor.temperature
-        humidity = sensor.humidity
-        print("Temperature: {}*C   Humidity: {}% ".format(temp, humidity))
-    except RuntimeError as error:
-        print(error.args[0])
-        time.sleep(2.0)
-        continue
-    except Exception as error:
-        sensor.exit()
-        raise error
-    time.sleep(2.0)
+class DHT11:
+    def __init__(self) -> None:
+        self.DHT_SENSOR = adafruit_dht.DHT11
+        self.DHT_PIN = 17
+
+    def read(self):
+        humidity, temperature = Adafruit_DHT.read_retry(self.DHT_SENSOR, self.DHT_PIN)
+        return humidity, temperature
+
+    def read_str(self):
+        humidity, temperature = self.read()
+        return "Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity)
