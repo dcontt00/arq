be855f3173212d04c8447ff307006a05ae293bad
dcontt00 <dcontt00@estudiantes.unileon.es>
2022-11-21 16:37:05 +0100

    creada clase database

--- backend/src/database.py
+++ backend/src/database.py
@@ -4,57 +4,48 @@ import os
 DB = "../db/database.sqlite"
 
 
-def create_db_file():
-    """creates a sqlite database file in db folder"""
-    if not os.path.exists(DB):
-        os.mkdir("../db")
-        f = open(DB, "w+")
-        f.close()
+class Database:
+    def __init__(self) -> None:
+        if not os.path.exists(DB):
+            os.mkdir("../db")
+            f = open(DB, "w+")
+            f.close()
 
+        self.conn = self.create_connection(DB)
+        self.create_table()
 
-def create_connection(db_file):
-    """create a database connection to a SQLite database"""
-    conn = None
-    try:
-        conn = sqlite3.connect(db_file)
+    def create_connection(self, db_file):
+        """create a database connection to a SQLite database"""
+        conn = None
+        try:
+            conn = sqlite3.connect(db_file)
+            return conn
+        except Exception as e:
+            print(e)
+
         return conn
-    except Exception as e:
-        print(e)
 
-    return conn
+    def create_table(self):
+        """create a table from the create_table_sql statement"""
 
+        sql_create_table = """ CREATE TABLE IF NOT EXISTS data (
+                                            id integer PRIMARY KEY,
+                                            date datetime DEFAULT CURRENT_TIMESTAMP,
+                                            temperature real NOT NULL,
+                                            humidity real NOT NULL,
+                                            soil_moisture integer NOT NULL
+                                        ); """
 
-def create_table():
-    """create a table from the create_table_sql statement"""
+        c = self.conn.cursor()
+        c.execute(sql_create_table)
 
-    sql_create_table = """ CREATE TABLE IF NOT EXISTS data (
-                                        id integer PRIMARY KEY,
-                                        date datetime DEFAULT CURRENT_TIMESTAMP,
-                                        temperature real NOT NULL,
-                                        humidity real NOT NULL,
-                                        soil_moisture integer NOT NULL
-                                    ); """
+    def add_data(self, temperature, humidity, soil_moisture):
+        """add data to the database"""
+        sql = """ INSERT INTO data(temperature, humidity, soil_moisture)
+                VALUES(?,?,?) """
 
-    # create a database connection
-    conn = create_connection(DB)
+        data = [temperature, humidity, soil_moisture]
 
-    c = conn.cursor()
-    c.execute(sql_create_table)
-
-
-def add_data(temperature, humidity, soil_moisture):
-    """add data to the database"""
-    sql = """ INSERT INTO data(temperature, humidity, soil_moisture)
-              VALUES(?,?,?) """
-
-    data = [temperature, humidity, soil_moisture]
-    conn = create_connection(DB)
-    cur = conn.cursor()
-    cur.execute(sql, data)
-    conn.commit()
-    conn.close()
-
-
-create_db_file()
-create_table()
-add_data(1, 2, 3)
+        cur = self.conn.cursor()
+        cur.execute(sql, data)
+        self.conn.commit()
