0962268058d415746408668018ee2a31629e0709
dcontt00 <dcontt00@estudiantes.unileon.es>
2022-11-21 16:54:33 +0100

    cambios

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from dht11 import DHT11
+from time import sleep
 
 # from soil_moisture import SoilMoisture
 from database import Database
@@ -8,6 +9,8 @@ app = FastAPI()
 dh11 = DHT11()
 # soilMoisture = SoilMoisture()
 db = Database()
+
+MINS_TO_UPDATE = 1
 
 
 @app.get("/")
@@ -20,14 +23,13 @@ async def get_data():
     """Gets data from all sensors
 
     Returns:
-        _type_: _description_
+        dict: containing temperature, humidity, soil_moisture.
+            Example:{"temperature": 20.0,"humidity": 50.0,"soil_moisture": 0}
     """
 
     """ temperature, humidity = dh11.read()
     soil_moisture = soilMoisture.read() """
-    temperature = 22
-    humidity = 0.56
-    soil_moisture = 1
+    temperature, humidity, soil_moisture = test_data()
 
     return {
         "temperature": temperature,
@@ -48,5 +50,16 @@ async def get_historical_data():
 
 def periodic_data():
     """get data from sensors and add it to the database"""
+    while True:
+        """temperature, humidity = dh11.read()
+        soil_moisture = soilMoisture.read()"""
+        temperature, humidity, soil_moisture = test_data()
+        db.add_data(temperature, humidity, soil_moisture)
+        sleep(MINS_TO_UPDATE * 60)
 
-    pass
+
+def test_data():
+    return 20.0, 50.0, 0
+
+
+periodic_data()
