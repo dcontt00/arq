40351698c07094f458a15db0c6c0feca96a3a141
dcontt00 <dcontt00@estudiantes.unielon.es>
2022-11-24 12:35:51 +0100

    added light sensor

---
+++ backend/src/light_sensor.py
@@ -0,0 +1,21 @@
+import time
+import RPi.GPIO as GPIO
+
+
+class LightSensor:
+    def __init__(self, pin) -> None:
+        self._pin = pin
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(pin, GPIO.IN)
+
+    @property
+    def pin(self) -> int:
+        return self._pin
+
+    def read(self) -> int:
+        """_summary_
+
+        Returns:
+           int: 0 is wet and 1 is dry
+        """
+        return GPIO.input(self.pin)
