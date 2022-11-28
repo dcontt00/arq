0dcacc9bcc2487dccd16849b2225bd8d40c7dbe4
dcontt00 <dcontt00@estudiantes.unielon.es>
2022-11-28 09:58:30 +0100

    añadido a test

--- backend/src/test.py
+++ backend/src/test.py
@@ -2,6 +2,7 @@ from dht11 import test_dht11
 from light_sensor import test_light_sensor
 from soil_moisture import test_soil_moisture
 from time import sleep
+from relay import test_relay
 
 
 while True:
@@ -9,6 +10,7 @@ while True:
         test_dht11()
         test_light_sensor()
         test_soil_moisture()
+        test_relay()
         print()
         sleep(3)
     except KeyboardInterrupt:
