dcdcd025c7e15e0facbafd222c5af3932fd70a75
dcontt00 <dcontt00@estudiantes.unielon.es>
2022-12-05 11:18:25 +0100

    cambios

--- backend/src/camera.py
+++ backend/src/camera.py
@@ -5,16 +5,15 @@ import os
 PIC_PATH = "../data/pics/"
 if not os.path.exists("../data/pics"):
     os.makedirs("../data/pics")
+picam2 = Picamera2()
+config = picam2.create_still_configuration()
+picam2.configure(config)
 
 
 def save_picture() -> None:
     date = time.strftime("%Y-%m-%d_%H:%M:%S")
-    picam2 = Picamera2()
-    try:
 
-        config = picam2.create_still_configuration()
-        picam2.configure(config)
-        picam2.start_and_capture_file(PIC_PATH + f"{date}.jpg")
-    finally:
-        picam2.stop()
+    picam2.start()
+
+    picam2.capture_file(PIC_PATH + f"{date}.jpg")
     return PIC_PATH + f"{date}.jpg"
