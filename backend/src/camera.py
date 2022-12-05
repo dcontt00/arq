d8837fb186ecf792034587c65c2018550229ebd5
dcontt00 <dcontt00@estudiantes.unielon.es>
2022-12-05 10:24:42 +0100

    delay

--- backend/src/camera.py
+++ backend/src/camera.py
@@ -12,6 +12,7 @@ class Camera:
         picam2 = Picamera2()
         preview_config = picam2.create_preview_configuration(main={"size": (800, 600)})
         picam2.configure(preview_config)
+        time.sleep(2)
 
         picam2.start_preview(Preview.QTGL)
         picam2.start()
