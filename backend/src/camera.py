7eaed80bb93b23a3e09371f09321f788670ce788
dcontt00 <dcontt00@estudiantes.unielon.es>
2022-12-05 10:26:57 +0100

    cambios

--- backend/src/camera.py
+++ backend/src/camera.py
@@ -10,15 +10,12 @@ class Camera:
         if not os.path.exists("../data/pics"):
             os.makedirs("../data/pics")
         picam2 = Picamera2()
-        preview_config = picam2.create_preview_configuration(main={"size": (800, 600)})
-        picam2.configure(preview_config)
-        time.sleep(2)
-
-        picam2.start_preview(Preview.QTGL)
+        config = picam2.create_still_configuration()
+        picam2.configure(config)
         picam2.start()
 
     def take_picture(self) -> None:
         return picam2.capture()
 
     def save_picture(self) -> None:
-        picam2.capture(PIC_PATH + "test.jpg")
+        picam2.capture_file(PIC_PATH + "test.jpg")
