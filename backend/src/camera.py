71dfee32b83a76381e1f1c3529d69f02ce0715bd
dcontt00 <dcontt00@estudiantes.unielon.es>
2022-12-05 10:34:58 +0100

    fix

--- backend/src/camera.py
+++ backend/src/camera.py
@@ -9,20 +9,16 @@ class Camera:
     def __init__(self) -> None:
         if not os.path.exists("../data/pics"):
             os.makedirs("../data/pics")
-        self._picam2 = Picamera2()
-        config = self._picam2.create_still_configuration()
-        self._picam2.configure(config)
-        self._picam2.start()
-
-    @property
-    def picam2(self) -> Picamera2:
-        return self._picam2
+        self.picam2 = Picamera2()
+        config = self.picam2.create_still_configuration()
+        self.picam2.configure(config)
+        self.picam2.start()
 
     def take_picture(self) -> None:
-        return picam2.capture()
+        return self.picam2.capture()
 
     def save_picture(self) -> None:
-        picam2.capture_file(PIC_PATH + "test.jpg")
+        self.picam2.capture_file(PIC_PATH + "test.jpg")
 
     def stop(self) -> None:
-        picam2.stop()
+        self.picam2.stop()
