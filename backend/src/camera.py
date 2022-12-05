c3317e8572cccae544323ff847d773d405e7a272
dcontt00 <dcontt00@estudiantes.unielon.es>
2022-12-05 10:20:01 +0100

    añadida ruta pics

--- backend/src/camera.py
+++ backend/src/camera.py
@@ -1,16 +1,20 @@
 import time
 from picamera2 import Picamera2, Preview
+import os
 
-picam2=Picamera2()
+PIC_PATH = "../data/pic/"
 
-preview_config=picam2.create_preview_configuration(main={"size":(800,600)})
-picam2.configure(preview_config)
 
-picam2.start_preview(Preview.QTGL)
-picam2.start()
-time.sleep(2)
+class Camera:
+    def __init__(self) -> None:
+        if not os.path.exists("../data/pics"):
+            os.mkdir("../data/pics")
+        picam2 = Picamera2()
+        preview_config = picam2.create_preview_configuration(main={"size": (800, 600)})
+        picam2.configure(preview_config)
 
-metadata=picam2.capture_file("test.jpg")
-print(metadata)
+        picam2.start_preview(Preview.QTGL)
+        picam2.start()
 
-picam2.close()
+    def take_picture(self) -> None:
+        picam2.capture(PIC_PATH + "test.jpg")
