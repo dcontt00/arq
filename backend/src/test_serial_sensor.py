3e235489d93c0ed43a0be8194a96d31090fbe472
rreisr00 <rreisr00@estudiantes.unileon.es>
2022-12-19 02:49:04 -0800

    added test for the sensors

--- backend/src/test_serial_sensor.py
+++ backend/src/test_serial_sensor.py
@@ -1,7 +1,7 @@
 import serial
 from time import sleep
 
-ser = serial.Serial ("/dev/ttyS0", 9600)    #Open port with baud rate
+ser = serial.Serial ("/dev/serial0", 9600)    #Open port with baud rate
 while True:
     received_data = ser.read()              #read serial port
     sleep(0.03)
