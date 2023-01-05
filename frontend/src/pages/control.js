f59993276035e9e9d798b95365681d24578569b5
dflorr00 <dflorr00@estudiantes.unileon.es>
2023-01-06 00:19:58 +0100

    Establecidos limites en valores de sala de control

--- frontend/src/pages/control.js
+++ frontend/src/pages/control.js
@@ -15,11 +15,12 @@ import LightbulbBorder from '@mui/icons-material/LightbulbSharp';
 import Lightbulb from '@mui/icons-material/LightbulbOutlined';
 
 export default function Control() {
-    const [HAir, setAir] = useState(40);
+    const [HAir, setAir] = useState(3);
     const [HGnd, setGnd] = useState(19);
     const [Temp, setTemp] = useState(20);
     function handlerLight(){
     }
+
     return(
     <div>
       <AppBar/>
@@ -44,20 +45,20 @@ export default function Control() {
                 <Typography component="h1" variant="h5">Panel de control</Typography>
               </Grid>
               {/*Humedad del aire*/}
-              <Grid item lg={3} xs={3} align="center" mt="10"><Button sx={{ mt: 3, mb: 2 }} onClick={()=>setAir(HAir-1)}><RemoveIcon/></Button></Grid>
+              <Grid item lg={3} xs={3} align="center" mt="10"><Button sx={{ mt: 3, mb: 2 }} onClick={HAir>0 ? ()=>setAir(HAir-1): undefined}><RemoveIcon/></Button></Grid>
               <Grid item lg={6} xs={6} align="center" mt="10"><TextField margin="normal" value={HAir} fullWidth  label="Humedad del aire" 
                 InputProps={{startAdornment: <InputAdornment position="start">%</InputAdornment>}}/></Grid>
-              <Grid item lg={3} xs={3} align="center" mt="10"><Button sx={{ mt: 3, mb: 2 }} onClick={()=>setAir(HAir+1)}><AddIcon/></Button></Grid>
+              <Grid item lg={3} xs={3} align="center" mt="10"><Button sx={{ mt: 3, mb: 2 }} onClick={HAir<100 ? ()=>setAir(HAir+1): undefined}><AddIcon/></Button></Grid>
               {/*Humedad del suelo*/}
-              <Grid item lg={3} xs={3} align="center" mt="10"><Button sx={{ mt: 3, mb: 2 }} onClick={()=>setGnd(HGnd+1)}><RemoveIcon/></Button></Grid>
+              <Grid item lg={3} xs={3} align="center" mt="10"><Button sx={{ mt: 3, mb: 2 }} onClick={HGnd>0 ? ()=>setGnd(HGnd-1): undefined}><RemoveIcon/></Button></Grid>
               <Grid item lg={6} xs={6} align="center" mt="10"><TextField margin="normal" value={HGnd} fullWidth label="Humedad del suelo"
                 InputProps={{startAdornment: <InputAdornment position="start">%</InputAdornment>}}/></Grid>
-              <Grid item lg={3} xs={3} align="center" mt="10"><Button sx={{ mt: 3, mb: 2 }} onClick={()=>setGnd(HGnd+1)}><AddIcon/></Button></Grid>    
+              <Grid item lg={3} xs={3} align="center" mt="10"><Button sx={{ mt: 3, mb: 2 }} onClick={HGnd<100 ? ()=>setGnd(HGnd+1): undefined}><AddIcon/></Button></Grid>    
               {/*Temperatura*/}
-              <Grid item lg={3} xs={3} align="center" mt="10"><Button sx={{ mt: 3, mb: 2 }} onClick={()=>setTemp(Temp+1)}><RemoveIcon/></Button></Grid>
+              <Grid item lg={3} xs={3} align="center" mt="10"><Button sx={{ mt: 3, mb: 2 }} onClick={Temp>0 ? ()=>setTemp(Temp-1): undefined}><RemoveIcon/></Button></Grid>
               <Grid item lg={6} xs={6} align="center" mt="10"><TextField margin="normal" value={Temp} fullWidth label="Temperatura"
                 InputProps={{startAdornment: <InputAdornment position="start">ºC</InputAdornment>}}/></Grid>
-              <Grid item lg={3} xs={3} align="center" mt="10"><Button sx={{ mt: 3, mb: 2 }} onClick={()=>setTemp(Temp+1)}><AddIcon/></Button></Grid>    
+              <Grid item lg={3} xs={3} align="center" mt="10"><Button sx={{ mt: 3, mb: 2 }} onClick={Temp<100 ? ()=>setTemp(Temp+1): undefined}><AddIcon/></Button></Grid>    
               {/*Iluminación*/}
               <Grid item lg={12} xs={12} align="center" mt="10"><Checkbox onChange={handlerLight} icon={<Lightbulb fontSize="large"/>} checkedIcon={<LightbulbBorder fontSize="large"/>} /></Grid>     
             </Grid>
--- frontend/src/pages/data.js
+++ frontend/src/pages/data.js
@@ -3,7 +3,7 @@ import AppBar from "../components/Appbar";
 import Grid from "@mui/material/Grid";
 import {Typography} from "@mui/material";
 import Datos from "../components/Datos";
-import {Bar} from "react-chartjs-2"
+import { Bar } from 'react-chartjs-2';
 
 export default function Data(){
   const data={
@@ -22,6 +22,7 @@ export default function Data(){
     maintainAspectRatio: false,
     responsive: true
   }
+
   return(
    <div>
       <AppBar/>
@@ -29,7 +30,7 @@ export default function Data(){
       <Grid container spacing={2} width={"99%"} sx={{mt:"5",ml:"5"}}>
         {/* Ultima foto del invernadero */}
         <Grid item lg={7}>
-          <Bar data={data} options={opciones}/>
+          {/*<Bar data={data}/>*/}
           <img src="planta.jpeg" alt="imagen" width={"100%"} />
         </Grid>
         {/* Datos de los invernaderos */}
