3b1060bc4c564c80216618a4aa03832541f88299
dflorr00 <dflorr00@estudiantes.unileon.es>
2023-01-06 17:18:29 +0100

    Implementada la grafica en el panel de datos

--- frontend/src/pages/dataLook.js
+++ frontend/src/pages/dataLook.js
@@ -3,39 +3,57 @@ import AppBar from "../components/Appbar";
 import Grid from "@mui/material/Grid";
 import {Typography} from "@mui/material";
 import Datos from "../components/Datos";
-import { Bar } from 'react-chartjs-2';
 import Paper from '@mui/material/Paper';
 import CssBaseline from '@mui/material/CssBaseline';
 
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar} from 'react-chartjs-2';
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+
 export default function DataLook(){
-  const data={
-    labels: ['Red', 'Orange', 'Blue'],
-    // datasets is an array of objects where each object represents a set of data to display corresponding to the labels above. for brevity, we'll keep it at one object
-    datasets: [{
-      label: 'Popularity of colours',
-      data: [55, 23, 96],// you can set indiviual colors for each bar
+  const data = {
+    labels:['Humedad del aire','Humedad del suelo','Temperatura','Luminosidad'],
+    datasets:[{
+      label:'Humedad del aire',
       backgroundColor: [
-        'rgba(255, 255, 255, 0.6)',
-        'rgba(255, 255, 255, 0.6)',
-        'rgba(255, 255, 255, 0.6)'
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
       ],
+      borderColor: 'black',
       borderWidth: 1,
-    }]
+      hoverBackgroundColor:'rgba(0,255,0,0.2)',
+      hoverBorderColor:'#FF0000',
+      data: [100, 75, 20, 40]
+    }] 
   };
-  const opciones ={
-    maintainAspectRatio: false,
-    responsive: true
-  }
 
   return(
    <div>
       <AppBar/>
       <Typography fontSize="25" align="center" sx={{mt:"10",mb:"10"}}>INFORMACIÓN BÁSICA</Typography>
       <CssBaseline />
-      <Grid container sx={{ml:"7%", height: '80vh', width:"90%"}}>
+      <Grid container >
         {/* Grafica de los datos */}   
         <Grid item lg={6} xs={false} sm={4} md={7} component={Paper} square>
-          {/*<Bar data={data} options={opciones}/>*/}
+          <Bar data={data} />
         </Grid>
         {/* Tomar una foto */}
         <Grid item lg={6} xs={6} component={Paper} square>
@@ -45,14 +63,3 @@ export default function DataLook(){
     </div>
   );
 }
-
-{/*labels:['Humedad del aire','Humedad del suelo','Temperatura','Luminosidad'],
-    datasets:[{
-      label:'Humedad del aire',
-      backgroundColor:'rgba(0,255,0,1)',
-      borderColor:'black',
-      borderWidth: 1,
-      hoverBackgroundColor:'rgba(0,255,0,0.2)',
-      hoverBorderColor:'#FF0000',
-      data: [100, 75, 20, 40]
-    }] */}
