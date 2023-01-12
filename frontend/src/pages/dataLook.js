c9b1f2d43a9a1e3150db57e7f0e45e9e965e0c5e
dcontt00 <dcontt00@estudiantes.unielon.es>
2023-01-12 11:10:11 +0100

    conexion con backend

--- frontend/src/pages/dataLook.js
+++ frontend/src/pages/dataLook.js
@@ -1,12 +1,14 @@
 import * as React from "react";
 import AppBar from "../components/Appbar";
 import Grid from "@mui/material/Grid";
-import {Typography} from "@mui/material";
+import { Typography } from "@mui/material";
 import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
-import { Line} from 'react-chartjs-2';
-import {lecturas} from "../components/lecturas";
+import { Line } from 'react-chartjs-2';
+import { lecturas } from "../components/lecturas";
 import { Box } from "@mui/system";
+import { useEffect, useState } from "react";
+import Axios from "axios";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -28,54 +30,116 @@ ChartJS.register(
   Legend
 );
 
-export default function DataLook(){
-  const hair = lecturas.hair;
-  const hgnd = lecturas.hgnd;
-  const temp = lecturas.temp;
-  const lum = lecturas.lum;
-  const data = {
-    labels:['T1','T2','T3','T4'],
-    datasets:[{
-      label:'Humedad del aire',
+export default function DataLook() {
+  const [datos, setDatos] = useState({
+    labels: ['0', '0'],
+    datasets: [{
+      label: 'Humedad del aire',
       borderColor: 'rgb(0, 0, 255)',
       backgroundColor: 'rgba(100, 100, 235)',
-      data: hair
-    },{
-      label:'Humedad del suelo',
+      data: [0, 0]
+    }, {
+      label: 'Humedad del suelo',
       borderColor: 'rgb(0, 255, 0)',
       backgroundColor: 'rgba(100, 255, 100)',
-      data: hgnd
-    },{
-      label:'Temperatura',
+      data: [0, 0]
+    }, {
+      label: 'Temperatura',
       borderColor: 'rgb(255, 99, 132)',
       backgroundColor: 'rgba(255, 99, 132, 0.5)',
-      data: temp
-    },{
-      label:'Iluminación',
+      data: [0, 0]
+    }, {
+      label: 'Iluminación',
       borderColor: 'rgb(255, 255, 0)',
       backgroundColor: 'rgba(255, 255, 100)',
-      data: lum
+      data: [0, 0]
     }]
-  };
+  });
 
-  return(
-   <div>
-      <AppBar/>
-      <Typography fontSize="25" align="center" sx={{mt:1,mb:1}}>INFORMACIÓN BÁSICA</Typography>
+  const [temp, setTemp] = useState(0);
+  const [humidity, setHumidity] = useState(0);
+  const [light, setLight] = useState(0);
+  const [soil_moisture, setSoil_moisture] = useState(0);
+
+
+  useEffect(() => {
+    Axios({
+      url: "http://localhost:8000/historical",
+    })
+      .then((response) => {
+
+        // Get an array of temperatures from an array of JSON objects
+        var temps = []
+        var hums = []
+        var soil_moistures = []
+        var dates = []
+        response.data.forEach(element => {
+          temps.push(element.temp)
+          hums.push(element.humidity)
+          soil_moistures.push(element.soil_moisture)
+          dates.push(element.date)
+        });
+
+        var dat = {
+          labels: dates,
+          datasets: [{
+            label: 'Humedad del aire',
+            borderColor: 'rgb(0, 0, 255)',
+            backgroundColor: 'rgba(100, 100, 235)',
+            data: hums
+          }, {
+            label: 'Humedad del suelo',
+            borderColor: 'rgb(0, 255, 0)',
+            backgroundColor: 'rgba(100, 255, 100)',
+            data: soil_moistures
+          }, {
+            label: 'Temperatura',
+            borderColor: 'rgb(255, 99, 132)',
+            backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            data: temps
+          }, {
+            label: 'Iluminación',
+            borderColor: 'rgb(255, 255, 0)',
+            backgroundColor: 'rgba(255, 255, 100)',
+            data: [0, 1, 0, 0, 1]
+          }]
+        };
+        setDatos(dat);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    Axios({
+      url: "http://localhost:8000/data",
+    })
+      .then((response) => {
+        setTemp(response.data.temperature)
+        setHumidity(response.data.humidity)
+        setLight(response.data.light)
+        setSoil_moisture((response.data.soil_moisture1 + response.data.soil_moisture2) / 2)
+      }, [setDatos, setTemp, setHumidity, setLight, setSoil_moisture]);
+  });
+
+
+  return (
+    <div>
+      <AppBar />
+      <Typography fontSize="25" align="center" sx={{ mt: 1, mb: 1 }}>INFORMACIÓN BÁSICA</Typography>
       <Grid container justifyContent="center">
-        {/* Grafica de los datos */}   
-        <Grid item lg={5} component={Paper} sx={{mr:10, mt:7}}>
+        {/* Grafica de los datos */}
+        <Grid item lg={5} component={Paper} sx={{ mr: 10, mt: 7 }}>
           <Box>
-            <Line data={data} />
+            <Line data={datos} />
           </Box>
         </Grid>
         {/* Datos */}
-        <Grid item lg={5} component={Paper} sx={{mt:7,backgroundColor:"lightGrey"}}>
-          <Stack spacing={2}  sx={{ml:2,mr:2}}>
-            <Paper sx={{mt:1.5}}><Typography sx={{ml:5,mt:1.5,mb:1.5}}>Humedad del aire: {hair[hair.length-1]}</Typography></Paper>
-            <Paper><Typography sx={{ml:5,mt:1.5,mb:1.5}}>Temperatura: {hgnd[hgnd.length-1]}</Typography></Paper>
-            <Paper><Typography sx={{ml:5,mt:1.5,mb:1.5}}>Humedad del suelo: {temp[temp.length-1]}</Typography></Paper>
-            <Paper><Typography sx={{ml:5,mt:1.5,mb:1.5}}>Luminosidad: {lum[lum.length-1]}</Typography></Paper>
+        <Grid item lg={5} component={Paper} sx={{ mt: 7, backgroundColor: "lightGrey" }}>
+          <Stack spacing={2} sx={{ ml: 2, mr: 2 }}>
+            <Paper sx={{ mt: 1.5 }}><Typography sx={{ ml: 5, mt: 1.5, mb: 1.5 }}>Humedad del aire: {humidity}</Typography></Paper>
+            <Paper><Typography sx={{ ml: 5, mt: 1.5, mb: 1.5 }}>Temperatura: {temp}</Typography></Paper>
+            <Paper><Typography sx={{ ml: 5, mt: 1.5, mb: 1.5 }}>Humedad del suelo: {soil_moisture}</Typography></Paper>
+            <Paper><Typography sx={{ ml: 5, mt: 1.5, mb: 1.5 }}>Luminosidad: {light}</Typography></Paper>
           </Stack>
         </Grid>
       </Grid>
