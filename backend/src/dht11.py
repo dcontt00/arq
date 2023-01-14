761e8dd84b4602c844438f7b8a49ad156050dd31
dcontt00 <dcontt00@estudiantes.unielon.es>
2023-01-14 12:32:03 +0100

    cambiada libreria

--- backend/src/dht11.py
+++ backend/src/dht11.py
@@ -1,21 +1,29 @@
-import Adafruit_DHT
-import time
+import dht11
 
 
 class DHT11:
     def __init__(self, pin: int) -> None:
-        self.DHT_SENSOR = Adafruit_DHT.DHT11
         self.DHT_PIN = pin
+        self.sensor = dht11.DHT11(pin=14)
+
+    @property
+    def sensor(self):
+        return self.sensor
 
     def read(self):
-        humidity, temperature = Adafruit_DHT.read_retry(self.DHT_SENSOR, self.DHT_PIN)
+        result = self.sensor.read()
+        humidity = result.humidity
+        temperature = result.temperature
+
         return humidity, temperature
 
     def read_str(self):
-        humidity, temperature = self.read()
+        result = self.sensor.read()
+        humidity = result.humidity
+        temperature = result.temperature
         return "Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     dht11 = DHT11(18)
     print("DHT11: " + dht11.read_str())
